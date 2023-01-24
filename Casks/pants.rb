87e015b81f0bd94b1dbadabf796b5200aa064f89
Andreas Stenius <andreas.stenius@imanage.com>
2023-01-24 15:25:59 -0500

    no need to track the sha sums for linux

--- Casks/pants.rb
+++ Casks/pants.rb
@@ -18,8 +18,9 @@ cask "pants" do
     sha256 arm: "79ba5efeb606b1d3dc13d810003a7c99d418e6e977ec65860ba6ad131f747928",
            intel: "c5bb6b99319a374b1e97dba17558e981c6e050b77823ad57ec0211a0efc366ce"
   else
-    sha256 arm: "ab062b11b18e96e1877c69946a807eaa2ac2ca8e96902a377e055bfc3684a77b",
-           intel: "f70a78a2470f909d42f6b306a7e96e270d1eaabeb19778007c20e2e13ba5c696"
+    # Casks not supported on Linux: https://github.com/Linuxbrew/brew/issues/742
+    # sha256 arm: "...",
+    #        intel: "..."
   end
   url "https://github.com/pantsbuild/scie-pants/releases/download/v#{version}/scie-pants-#{Utils.os}-#{Utils.arch}",
       verified: "https://github.com/pantsbuild/"
