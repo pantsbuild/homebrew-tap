2346a00b881b0063bbeaa2d60c3a774d8f1f175a
Andreas Stenius <andreas.stenius@imanage.com>
2023-01-28 19:57:37 -0500

    Tweaks.

--- bump_version.sh
+++ bump_version.sh
@@ -1,4 +1,5 @@
 #/bin/env bash
+set -eufo pipefail
 
 # A script to update the version and shas
 version=${1/v/}
@@ -9,14 +10,21 @@ if [ "$version" == "" ]; then
   exit 1
 fi
 
+inline=(-i)
+if [ $(uname -o) == Darwin ]; then
+    inline[1]=''
+fi
+
 for tuple in 'arm: aarch64' 'intel: x86_64'; do
   declare -a platform=($tuple)
   tag=${platform[0]}
   arch=${platform[1]}
   url="https://github.com/pantsbuild/scie-pants/releases/download/v${version}/scie-pants-macos-${arch}.sha256"
   sha=$(curl -Ls ${url} | cut -f 1 -d ' ')
-  sed -i '' "s/version \"[^\"]*\"/version \"${version}\"/" "${cask}"
-  sed -i '' "s/${tag} \"[0-9a-f]*\"/${tag} \"${sha}\"/" "${cask}"
+  sed -f - "${inline[@]}" "${cask}" <<EOF
+s/version "[^"]*"/version "${version}"/
+s/${tag} "[0-9a-f]*"/${tag} "${sha}"/
+EOF
 done
 
 echo "Updated ${cask}, please review the changes then commit and push--thanks!"
