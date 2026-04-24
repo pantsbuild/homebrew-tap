82b8d53c63dc0b5eb83791cc825069ec4bb0359c
sj <suresh@vicarasolutions.com>
2026-04-23 21:28:57 -0400

    Fixed crash and cleaned up with brew style

--- Casks/pants.rb
+++ Casks/pants.rb
@@ -1,37 +1,16 @@
 cask "pants" do
-  module Utils
-    @@os = OS.mac? ? "macos" : OS.kernel_name.downcase
-    @@arch = Hardware::CPU.arch.to_s.sub("arm64", "aarch64")
-    def self.os
-      @@os
-    end
-    def self.arch
-      @@arch
-    end
-    def self.binary
-      "scie-pants-#{@@os}-#{@@arch}"
-    end
-  end
-
   version "0.13.2"
-  if OS.mac?
-    sha256 arm: "a6f3231413ca1f793caffa621171a4b1a0158e7488cd0b5bb3e742cb99cc72a8",
-           intel: "Not"
-  else
-    # Casks not supported on Linux: https://github.com/Linuxbrew/brew/issues/742
-    # sha256 arm: "...",
-    #        intel: "..."
-  end
-  url "https://github.com/pantsbuild/scie-pants/releases/download/v#{version}/scie-pants-#{Utils.os}-#{Utils.arch}",
-      verified: "https://github.com/pantsbuild/"
+  sha256 "a6f3231413ca1f793caffa621171a4b1a0158e7488cd0b5bb3e742cb99cc72a8"
 
+  url "https://github.com/pantsbuild/scie-pants/releases/download/v#{version}/scie-pants-macos-aarch64",
+      verified: "github.com/pantsbuild/"
   name "Pants"
   desc "Fast, scalable, user-friendly build system for codebases of all sizes"
-  homepage "https://pantsbuild.org"
+  homepage "https://pantsbuild.org/"
 
-  depends_on arch: [:arm64, :x86_64]
+  depends_on arch: :arm64
 
-  binary Utils.binary, target: "pants"
+  binary "scie-pants-macos-aarch64", target: "pants"
 
   preflight do
     target = config.binarydir / "pants"
@@ -42,6 +21,9 @@ cask "pants" do
   end
 
   postflight do
-    Quarantine.release!(download_path: "#{caskroom_path}/#{version}/#{Utils.binary}") if Quarantine.available?
+    if Quarantine.available?
+      Quarantine.release!(download_path: caskroom_path.join(version,
+                                                            "scie-pants-macos-aarch64"))
+    end
   end
 end
